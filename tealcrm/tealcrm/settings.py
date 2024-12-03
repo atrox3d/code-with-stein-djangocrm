008105e567d6dec10c25e4751496e8008a5211a3
atrox3d <atrox3d@gmail.com>
2024-12-03 09:30:28 +0100

    add base templte

--- tealcrm/tealcrm/settings.py
+++ tealcrm/tealcrm/settings.py
@@ -47,7 +47,9 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # enable vscode simple browser
+    # https://stackoverflow.com/a/77771014
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'tealcrm.urls'
@@ -122,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# X_FRAME_OPTIONS = 'SAMEORIGIN'
